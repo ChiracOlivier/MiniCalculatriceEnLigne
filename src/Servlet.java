e4e94b931270bc081eac59ebabc72d796c386140
Chiracolivier <chiracolivier28@gmail.com>
2019-09-10 22:49:42 +0200

    add Calculator.java

--- src/Servlet.java
+++ src/Servlet.java
@@ -1,11 +1,22 @@
 import java.io.IOException;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    private static final String NOMBRE1
+    private static final String VUE = "/WEB-INF/index.jsp";
 
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+       Calculator calculator = new Calculator();
+       Int nombre1= request.getParameter(nombre1);
+       Int nombre2= request.getParameter(nombre2);
+
+
+       request.setAttribute(NOMBRE1, nombre1);
+        request.setAttribute(NOMBRE2, nombre2);
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 }
---
+++ web/WEB-INF/index.jsp
@@ -0,0 +1,34 @@
+<%@ page pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Inscription</title>
+  <link type="text/css" rel="stylesheet" href="style.css" />
+</head>
+<body>
+<form method="post" action="Servlet">
+  <fieldset>
+    <legend>Calculatrice</legend>
+    <p>Entrez vos nombres!!!.</p>
+
+    <label for="nombre1">Nombre1 </label>
+    <input type="number" id="nombre1" name="nombre1" value="<c:out value=""/>" size="20" maxlength="60" />
+    <span class="erreur">${form.erreurs['nombre1']}</span>
+    <span> ${ calculator.signe}</span>
+
+    <label for="nombre2">Nombre2 </label>
+    <input type="number" id="nombre2" name="nombre2" value="" size="20" maxlength="20" />
+    <span class="erreur">${form.erreurs['nombre2']}</span>
+
+
+
+    <input type="submit" value="GO" class="sansLabel" />
+    <br />
+
+    <p class="${empty form.erreurs ? 'succes' : 'erreur'}">${form.resultat}</p>
+  </fieldset>
+</form>
+</body>
+</html>
