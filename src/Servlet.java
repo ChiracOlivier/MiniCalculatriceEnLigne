da1ed5cbabdd6c5105705b95d27a193ff79eac28
Chiracolivier <chiracolivier28@gmail.com>
2019-09-10 22:56:57 +0200

    arranging Servlet

--- src/Servlet.java
+++ src/Servlet.java
@@ -1,18 +1,17 @@
 import java.io.IOException;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
-    private static final String NOMBRE1
     private static final String VUE = "/WEB-INF/index.jsp";
 
 
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
        Calculator calculator = new Calculator();
-       Int nombre1= request.getParameter(nombre1);
-       Int nombre2= request.getParameter(nombre2);
+       String nombre1= request.getParameter("nombre1");
+       String nombre2= request.getParameter("nombre2");
 
 
-       request.setAttribute(NOMBRE1, nombre1);
-        request.setAttribute(NOMBRE2, nombre2);
+       request.setAttribute("nombre1", nombre1);
+        request.setAttribute("nombre2", nombre2);
         this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
     }
 
