6eee44b92bb136b2cb00fd1e7e0bc5cadec6b185
Chiracolivier <chiracolivier28@gmail.com>
2019-09-11 06:05:15 +0200

    arranging index.jsp, creating and arranging indexo.jsp, arranging Calculator.java, Servlet.java and style.css.

--- src/Servlet.java
+++ src/Servlet.java
@@ -2,20 +2,38 @@ import java.io.IOException;
 
 public class Servlet extends javax.servlet.http.HttpServlet {
     private static final String VUE = "/WEB-INF/index.jsp";
-    boolean a=true;
-
-
+    private static final String VUEX= "/WEB-INF/indexo.jsp";
+    private Calculator calculator= new Calculator();
+    private Double resultat;
+    private double nombre1, nombre2;
+    private String operation="",number1="", number2="", resultatCompl="";
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-       Calculator calculator = new Calculator();
-       a=false;
-       String nombre1= request.getParameter("nombre1");
-       String nombre2= request.getParameter("nombre2");
+
+         operation= request.getParameter("operation");
+        number1= request.getParameter("nombre1");
+         number2= request.getParameter("nombre2");
+        if(!number1.equals(""))
+            nombre1 = Double.parseDouble(number1);
+         else
+             nombre1=0.0;
+         if(!number2.equals(""))
+             nombre2 = Double.parseDouble(number2);
+         else
+             nombre2=0.0;
 
 
+        resultat= calculator.result(operation, nombre1,nombre2);
+
+        if(resultat==null) {
+            resultatCompl = "operation impossible";
+        }else {
+            resultatCompl = String.valueOf(resultat);
+        }
+       request.setAttribute("operation", operation);
+       request.setAttribute("resultatCompl",resultatCompl);
        request.setAttribute("nombre1", nombre1);
         request.setAttribute("nombre2", nombre2);
-        request.setAttribute("a", a);
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        this.getServletContext().getRequestDispatcher( VUEX ).forward( request, response );
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
--- web/WEB-INF/index.jsp
+++ web/WEB-INF/index.jsp
@@ -1,34 +1,34 @@
 
+
+
 <%@ page pageEncoding="UTF-8" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8" />
-  <title>Inscription</title>
+  <title>Calculatrice En Ligne</title>
   <link type="text/css" rel="stylesheet" href="style.css" />
 </head>
 <body>
 <form method="post" action="Servlet">
   <fieldset>
     <legend>Calculatrice</legend>
-    <p>${ calculator.a ? 'Entrez vos nombres!!!': 'Rien'} </p>
+    <p>Entrez vos nombres!! </p>
 
-    <label for="nombre1">Nombre1 </label>
-    <input type="number" id="nombre1" name="nombre1" value="<c:out value=""/>" size="20" maxlength="60" />
-    <span class="erreur">${form.erreurs['nombre1']}</span>
-    <span> ${ calculator.signe}</span>
+    <input type="number" id="nombre1" name="nombre1"  placeholder="1er nombre" value="" size="20" maxlength="20" />
 
-    <label for="nombre2">Nombre2 </label>
-    <input type="number" id="nombre2" name="nombre2" value="" size="20" maxlength="20" />
-    <span class="erreur">${form.erreurs['nombre2']}</span>
+    <select id="operation" name="operation" required autofocus  >
+      <option value='+'> + </option>
+      <option value='-'> - </option>
+      <option value='*'> * </option>
+      <option value="/"> / </option>
+    </select>
 
+    <input type="number" id="nombre2" name="nombre2" placeholder="2ème nombre" value="" size="20" maxlength="20" />
 
-
-    <input type="submit" value="GO" class="sansLabel" />
+    <input type="submit" value="ENVOYER" class="sansLabel" />
     <br />
-
-    <p class="${empty form.erreurs ? 'succes' : 'erreur'}">${form.resultat}</p>
   </fieldset>
 </form>
 </body>
